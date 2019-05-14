2b3a5386849cdd232a7f8339f846d00a421d31ea
Tiaan Wolfaardt <sebastiaan.wolfaardt@gmail.com>
2019-05-14 22:11:00 +0200

    Added logic to the Decryption Class and some edits in filehandler

--- ProgrammingProject/Decryption.cs
+++ ProgrammingProject/Decryption.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 
 namespace ProgrammingProject
@@ -23,14 +24,55 @@ namespace ProgrammingProject
 
         private void btnExit_Click(object sender, EventArgs e)
         {
-            
+            Environment.Exit(0);
         }
 
         private void btnDecryption_Click_1(object sender, EventArgs e)
         {
-            string filename = txtBrowse.Text;
-            Filehandler fh = new Filehandler();
-            Decoding.
+            try 
+	        {	        
+		        string filename = txtBrowse.Text;
+                if (filename != null)
+	            {
+                    string messageDecrypted = "";
+                    Filehandler fh = new Filehandler();
+                    List<string> myList = new List<string>();
+                    myList = fh.Readfile(filename);
+                    int shiftKey = 1;
+
+                    foreach (string line in myList)
+                    {
+                        string lineToCheck = Decoding.Caesar(line,shiftKey);
+                        //We check if the string is english or not...
+                        if (Regex.IsMatch(lineToCheck,"^[a-zA-Z0-9]*$"))
+	                    {
+                            bool isSecret = Decoding.IsPalindrome(lineToCheck);
+                            Decoding.SendEmail(isSecret);
+                            messageDecrypted = messageDecrypted + " | " + lineToCheck;
+                            shiftKey = 1;
+	                    }
+                        else
+	                    {
+                            shiftKey += shiftKey;
+	                    }
+                        //return messageDecrypted;
+                    }
+                    DBAccess dba = new DBAccess();
+                    //
+                    //
+                    //Add the insert to database here...
+                    //
+                    //
+	            }
+	        }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            finally
+            {
+                fh.WriteToFile(filename);
+            }
         }
     }
 }
--- ProgrammingProject/Filehandler.cs
+++ ProgrammingProject/Filehandler.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
 using System.Windows.Forms;
 using System.IO;
 
 namespace ProgrammingProject
 {
-    public class Filehandler
+    public class FileHandler
     {
         FileStream stream;
         StreamReader reader;
@@ -42,7 +46,7 @@ namespace ProgrammingProject
                 MessageBox.Show(ioe.Message);
             }
             finally
-            {   
+            {
                 reader.Close();
                 //stream.Flush();
                 stream.Close();
@@ -52,6 +56,7 @@ namespace ProgrammingProject
         public void WriteToFile(string filename)
         {
             string dataToWrite = DateTime.Now.ToShortDateString();
+            Decoding.StringToBinary(dataToWrite);
             stream = new FileStream(filename, FileMode.Open, FileAccess.Write);
             writer = new StreamWriter(stream);
 
@@ -78,6 +83,6 @@ namespace ProgrammingProject
                 stream.Close();
             }
         }
-        
+
     }
 }
